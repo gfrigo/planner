57e3084b0779b000badf6217dc58768060178603
Gabriel Frigo <gfrigo.sena@gmail.com>
2024-07-09 10:33:35 -0300

    create-trip-entity

---
+++ src/main/java/com/rocketseat/planner/trip/Trip.java
@@ -0,0 +1,32 @@
+package com.rocketseat.planner.trip;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Trip {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String destination;
+    
+    @Column(name = "starts_at", nullable = false)
+    private LocalDateTime startAt;
+
+    @Column(name = "ends_at", nullable = false)
+    private LocalDateTime endsAt;
+
+    @Column(name = "is_confirmed", nullable = false)
+    private Boolean isConfirmed;
+
+    @Column(name = "owner_name", nullable = false)
+    private String ownerName;
+
+    @Column(name = "owner_email", nullable = false)
+    private String ownerEmail;
+}
