44e62cb47f17618e4de035a5ef662a3372fd1c22
Gabriel Frigo <gfrigo.sena@gmail.com>
2024-07-09 12:02:46 -0300

    participant-service

---
+++ src/main/java/com/rocketseat/planner/participant/ParticipantService.java
@@ -0,0 +1,15 @@
+package com.rocketseat.planner.participant;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParticipantService {
+    
+    public void registerParticipantsToTrip(List<String> paticipantsToEnvite, UUID tripId){}
+
+    public void triggerConfirmationEmailToParticipants(UUID tripId){}
+
+}
