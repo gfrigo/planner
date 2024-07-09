252fb7f7b923e1c22d26fe9cfe98d695e68e14ca
Gabriel Frigo <gfrigo.sena@gmail.com>
2024-07-09 12:01:08 -0300

    trip-entity-1.1

--- src/main/java/com/rocketseat/planner/trip/Trip.java
+++ src/main/java/com/rocketseat/planner/trip/Trip.java
@@ -1,13 +1,22 @@
 package com.rocketseat.planner.trip;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /*Entidade Trip é a representação da tabela trip*/
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Trip {
     
     @Id
@@ -31,4 +40,13 @@ public class Trip {
 
     @Column(name = "owner_email", nullable = false)
     private String ownerEmail;
+
+    public Trip(TripRequestPayload data){
+        this.destination = data.destination();
+        this.isConfirmed = false;
+        this.ownerEmail = data.owner_email();
+        this.ownerName = data.owner_name();
+        this.startAt = LocalDateTime.parse(data.starts_at(), DateTimeFormatter.ISO_DATE_TIME);
+        this.endsAt = LocalDateTime.parse(data.ends_at(), DateTimeFormatter.ISO_DATE_TIME);
+    }
 }
