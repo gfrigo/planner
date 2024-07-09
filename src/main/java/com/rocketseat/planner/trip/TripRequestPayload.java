1602124bf39601d0fbc98c20dea1af20a3ba8a89
Gabriel Frigo <gfrigo.sena@gmail.com>
2024-07-09 12:02:08 -0300

    trip-request-payload

---
+++ src/main/java/com/rocketseat/planner/trip/TripRequestPayload.java
@@ -0,0 +1,8 @@
+package com.rocketseat.planner.trip;
+
+import java.util.List;
+
+/*Record: Objetos que não são alterados*/
+/*Cria Getters e Setters de forma facilitada*/
+public record TripRequestPayload (String destination, String starts_at, String ends_at, List<String> emails_to_envite, String owner_name, String owner_email){
+}
