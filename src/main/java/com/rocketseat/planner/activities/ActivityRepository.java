e6a979112e16ff680f438a959d1b9c05083581c9
Gabriel Frigo <gfrigo.sena@gmail.com>
2024-07-12 18:28:13 -0300

    activities

---
+++ src/main/java/com/rocketseat/planner/activities/ActivityRepository.java
@@ -0,0 +1,8 @@
+package com.rocketseat.planner.activities;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActivityRepository extends JpaRepository<Activity, UUID>{
+}
